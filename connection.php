aba18cdb283594c8d612be0cdd84fd8776a57f28
joshreymagno <140252611+joshreymagno@users.noreply.github.com>
2023-07-22 20:45:19 +0800

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php
+$servername = "localhost";
+$database = "magno_project";
+$username = "root";
+$password = "";
+// Create connection username u316816729_garreta password Garreta@2023
+$db = mysqli_connect($servername, $username, $password, $database);
+// Check connection hbAZk18VnMPiB6rpABHV
+if (!$db) {
+    die("Connection failed: " . mysqli_connect_error());
+}
---
+++ sidebar.php
@@ -0,0 +1,75 @@
+<ul class="navbar-nav bg-gradient-success sidebar sidebar-dark accordion" id="accordionSidebar">
+
+            <!-- Sidebar - Brand -->
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="home.php">
+                <div class="sidebar-brand-icon rotate-n-15">
+                <i class="	fas fa-school"></i>
+                </div>
+                <div class="sidebar-brand-text mx-3">Student Information System<sup></sup></div>
+            </a>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider my-0">
+
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item">
+                <a class="nav-link" href="home.php">
+                    <i class="fa fa-home" aria-hidden="true"></i>
+                    <span>Home</span></a>
+            </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider">
+
+            <!-- Heading -->
+            <div class="sidebar-heading">
+                Interface
+            </div>
+
+            <!-- Nav Item - Pages Collapse Menu -->
+            <li class="nav-item active">
+                <a class="nav-link" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true"
+                    aria-controls="collapseTwo">
+                    <i class="fa fa-graduation-cap" aria-hidden="true"></i>
+                    <span>Student Categories</span>
+                </a>
+                <div id="collapseTwo" class="collapse show" aria-labelledby="headingTwo"
+                    data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <h6 class="collapse-header"></h6>
+                        <a class="collapse-item" href="students.php">Student</a>
+                        <a class="collapse-item active" href="subjects.php">Subject</a>
+                    </div>
+                </div>
+            </li>
+
+
+            <!-- Divider -->
+
+            <!-- Heading -->
+
+            <!-- Nav Item - Pages Collapse Menu -->
+
+            <!-- Nav Item - Charts -->
+            <li class="nav-item">
+                <a class="nav-link" href="exam.php">
+                    <i class="fas fa-fw fa-chart-area"></i>
+                    <span>Grades in Exam</span></a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="user.php">
+                    <i class="fa fa-user" aria-hidden="true"></i>
+                    <span>Users</span></a>
+            </li>
+
+            <!-- Nav Item - Tables -->
+
+            <!-- Divider -->
+
+            <!-- Sidebar Toggler (Sidebar) -->
+            <div class="text-center d-none d-md-inline">
+                <button class="rounded-circle border-0" id="sidebarToggle"></button>
+            </div>
+
+        </ul>
